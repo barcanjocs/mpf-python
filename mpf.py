fdc3e07ea3f0b3b32c164fef75bc4f00d2b31e5f
bruno <barcanjocs@outlook.com>
2023-07-31 15:30:26 +0100

    comp times

--- mpf.py
+++ mpf.py
@@ -10,15 +10,16 @@ import pandas as pd
 from mpf_run import mpf_run
 from settings import AlgoSettings
 import sklearn
+import time
 import helpers
 
 algo_settings = AlgoSettings()
 
 matrices_path = '../research/projects/A-MuSIC/data/score_vectors/'
 techniques = ['NetVLAD', 'CoHOG', 'HOG', 'CALC']
-dataset = 'fall'
-numTemplates = 1000
-margin=1
+dataset = '17places_night'
+numTemplates = 2000
+margin=10
 diff_matrices = []
 
 # Load difference matrices
@@ -35,15 +36,19 @@ if dataset == "17places_night" :
     for i in range(len(diff_matrices)) :
         diff_matrices[i] = diff_matrices[i][:2000, :2000]
 
+s = time.time()
 preds, qualities, scores = mpf_run(diff_matrices, algo_settings)
-
+f = time.time()
+e = ((f-s)*1000) / numTemplates
 selected_scores = np.array(scores).max(axis=1)
 
 labels = np.arange(numTemplates - len(selected_scores), numTemplates)
 
 p, r, auc = helpers.custom_label_pr_auc(preds, selected_scores, labels, margin)
 
-helpers.pr_auc_to_file(p, r, [auc], "./data/MPF_" + dataset + ".csv")
+print(auc, e, 4 * numTemplates)
+
+# helpers.pr_auc_to_file(p, r, [auc], "./data/MPF_" + dataset + ".csv")
 
 
 
